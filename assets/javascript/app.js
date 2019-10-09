a3f40f68705f36aebd259bd75aff0df670d7a8cd
Manuel R <mannyvazqsan1@icloud.com>
2019-10-09 11:27:55 -0400

    created basic html layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJTQSTWaDoMAFi-In5J5CjMidO1tskzpI",
+    authDomain: "happy-hour-d3a31.firebaseapp.com",
+    databaseURL: "https://happy-hour-d3a31.firebaseio.com",
+    projectId: "happy-hour-d3a31",
+    storageBucket: "",
+    messagingSenderId: "629203832179",
+    appId: "1:629203832179:web:5cc02d69ade2014ff44906"
+  };
+
+  
