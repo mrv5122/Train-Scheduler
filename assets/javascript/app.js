1264c5516351288b2b799a3809bdeb1b9771ca83
Manuel R <mannyvazqsan1@icloud.com>
2019-12-08 19:02:15 -0500

    firebase stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
-var config = {
+var firebaseConfig = {
   apiKey: "AIzaSyCJTQSTWaDoMAFi-In5J5CjMidO1tskzpI",
   authDomain: "happy-hour-d3a31.firebaseapp.com",
   databaseURL: "https://happy-hour-d3a31.firebaseio.com",
   projectId: "happy-hour-d3a31",
   storageBucket: "happy-hour-d3a31.appspot.com",
   messagingSenderId: "629203832179",
-  appId: "1:629203832179:web:5cc02d69ade2014ff44906"
+  appId: "1:629203832179:web:32d38dc4a1619073f44906",
+  measurementId: "G-WN7NNJ6SRE"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
-var trainInfo = database.ref("/trains");
+var trainInfo = database.ref("/");
 console.log(trainInfo);
 
 $(document).ready(function() {
@@ -105,7 +106,6 @@ $(document).ready(function() {
     }
 
     currTimeUpd();
-    dataUpdater();
     setInterval(currTimeUpd, 1000);
     setInterval(dataUpdater, 1000);
 
